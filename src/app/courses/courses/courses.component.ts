c999ca4011e40e39367d50f18711e0fa7e0153a1
= <=>
2023-10-18 15:12:26 -0300

    project gerence courses colletion with CRUD angular + spring

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit{
+
+  courses: Course[];
+
+  constructor(){
+    this.courses = [];
+  }
+
+  ngOnInit(): void {
+
+  }
+}
